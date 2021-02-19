c7a80e4647bccf998b01e8bb2a380730999ddd73
minarkhan <minarkhan1995@gmail.com>
2021-02-19 13:26:31 +0600

    initial file + model migration seed factory controller

---
+++ database/factories/ExamFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\exam;
+use Faker\Generator as Faker;
+
+$factory->define(exam::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
